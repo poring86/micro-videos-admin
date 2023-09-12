be8ac79fb9d4a550be052a05cc9eaa5c5f40f818
Matheus Lino <matheuslino86@gmail.com.br>
2023-09-12 18:52:17 -0400

    feat: :sparkles: Category in memory repository done

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,5 @@
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { IRepository } from "../../shared/repository/repository-interface";
+import { IRepository } from "../../shared/domain/repository/repository-interface";
 import { Category } from "./category.entity";
 
 export interface ICategoryRepository extends IRepository<Category, Uuid> {
