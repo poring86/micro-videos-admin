8cd0c3e678a2edbe357f3ce81752aba2352864fc
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-05 02:29:43 +0000

    feat: :sparkles: Categories controller provider

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
 
 import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.usecase';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.usecase';
+import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.usecase';
+import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.usecase';
+import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-category/list-category.usecase';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepo: CategorySequelizeRepository) { }
+
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase
+
+  @Inject(ListCategoriesUseCase)
+  private listUseCase: ListCategoriesUseCase
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
