640ea9516701494512715799690fc8a791818079
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-12 23:45:59 +0000

    feat: :sparkles: API data interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { WrapperDataInterceptor } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,5 +12,8 @@ async function bootstrap() {
   }))
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+
+  app.useGlobalInterceptors(new WrapperDataInterceptor())
+  await app.listen(3000)
 }
 bootstrap();
