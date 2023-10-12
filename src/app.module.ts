cc7f4b3efc706bc6e3f9d06b3488419e12dc70ca
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-12 21:28:29 +0000

    feat: :sparkles: Env dev and api request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from './nest-modules/categories-module/categories.module';
 import { DatabaseModule } from './nest-modules/database-module/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from './nest-modules/config-module/config.module';
 import { SharedModule } from './nest-modules/shared-module/shared.module';
 
 @Module({
