86bdd59c727fd7300ce08c008899d20707e931e4
Matheus Lino <matheuslino86@gmail.com.br>
2023-10-15 00:31:25 +0000

    feat: :sparkles: E2E testing helper

--- test/categories/create-category.e2e-spec.ts
+++ test/categories/create-category.e2e-spec.ts
@@ -1,26 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { CreateCategoryFixture } from '../../src/nest-modules/categories-module/testing/category-fixture';
 import { ICategoryRepository } from '../../src/core/category/domain/category.repository';
-
-import { AppModule } from '../../src/app.module';
-import { applyGlobalConfig } from '../../src/nest-modules/global-config';
 import { CATEGORY_PROVIDERS } from '../../src/nest-modules/categories-module/categories.providers';
+import { startApp } from '../../src/nest-modules/shared-module/testing/helpers';
 
 
 describe('CategoriesController (e2e)', () => {
-  let app: INestApplication;
+  const appHelper = startApp();
   let categoryRepo: ICategoryRepository;
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    applyGlobalConfig(app);
-    await app.init();
-    categoryRepo = app.get<ICategoryRepository>(
+    categoryRepo = appHelper.app.get<ICategoryRepository>(
       CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
     );
   });
@@ -30,13 +19,15 @@ describe('CategoriesController (e2e)', () => {
     test.each(arrange)(
       'when body is $send_data',
       async ({ send_data, expected }) => {
-        const res = await request(app.getHttpServer())
+        const res = await request(appHelper.app.getHttpServer())
           .post('/categories')
           .send(send_data)
           .expect(201);
 
         const keysInResponse = CreateCategoryFixture.keysInResponse;
         expect(Object.keys(res.body)).toStrictEqual(['data']);
+        expect(Object.keys(res.body.data)).toStrictEqual(keysInResponse)
+        const id = res.body.data.id
       },
     );
   });
