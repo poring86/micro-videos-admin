604bb1ba868eb2621bd967f3931807b179ed7be9
poring86 <matheuslino86a@gmail.com>
2024-04-02 00:02:19 +0000

    upload audio video media usecase

---
+++ src/core/video/application/upload-audio-video-medias/upload-audio-video-medias.use-case.ts
@@ -0,0 +1,68 @@
+import { IUseCase } from '@core/shared/application/use-case.interface';
+import { UploadAudioVideoMediaInput } from './upload-audio-video-media.input';
+import { IUnitOfWork } from '@core/shared/domain/repository/unit-of-work.interface';
+import { IVideoRepository } from '@core/video/domain/video.repository';
+import { IStorage } from '@core/shared/application/storage.interface';
+import { Video, VideoId } from '@core/video/domain/video.aggregate';
+import { NotFoundError } from '@core/shared/domain/errors/not-found.error';
+import { Trailer } from '@core/video/domain/trailer.vo';
+import { VideoMedia } from '@core/video/domain/video-media.vo';
+import { EntityValidationError } from '@core/shared/domain/validators/validation.error';
+
+export class UploadAudioVideoMediasUseCase
+  implements IUseCase<UploadAudioVideoMediaInput, UploadAudioVideoMediaOutput>
+{
+  constructor(
+    private uow: IUnitOfWork,
+    private videoRepo: IVideoRepository,
+    private storage: IStorage,
+  ) {}
+
+  async execute(
+    input: UploadAudioVideoMediaInput,
+  ): Promise<UploadAudioVideoMediaOutput> {
+    const video = await this.videoRepo.findById(new VideoId(input.video_id));
+    if (!video) {
+      throw new NotFoundError(input.video_id, Video);
+    }
+
+    const audioVideoMediaMap = {
+      trailer: Trailer,
+      video: VideoMedia,
+    };
+
+    const audioMediaClass = audioVideoMediaMap[input.field] as
+      | typeof Trailer
+      | typeof VideoMedia;
+    const [audioVideoMedia, errorAudioMedia] = audioMediaClass
+      .createFromFile({
+        ...input.file,
+        video_id: video.video_id,
+      })
+      .asArray();
+
+    if (errorAudioMedia) {
+      throw new EntityValidationError([
+        {
+          [input.field]: [errorAudioMedia.message],
+        },
+      ]);
+    }
+
+    audioVideoMedia instanceof Trailer && video.replaceTrailer(audioVideoMedia);
+    audioVideoMedia instanceof VideoMedia &&
+      video.replaceVideo(audioVideoMedia);
+
+    await this.storage.store({
+      data: input.file.data,
+      id: audioVideoMedia.raw_url,
+      mime_type: input.file.mime_type,
+    });
+
+    await this.uow.do(async () => {
+      return this.videoRepo.update(video);
+    });
+  }
+}
+
+export type UploadAudioVideoMediaOutput = void;
