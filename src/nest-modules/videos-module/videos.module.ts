b38696872a684e2ae0c2ddd6f8dd8f8f234e7c81
poring86 <matheuslino86a@gmail.com>
2024-07-13 20:47:19 +0000

    video consumer

--- src/nest-modules/videos-module/videos.module.ts
+++ src/nest-modules/videos-module/videos.module.ts
@@ -14,6 +14,7 @@ import { CastMembersModule } from '../cast-members-module/cast-members.module';
 import { VIDEOS_PROVIDERS } from './videos.providers';
 import { VideosController } from './videos.controller';
 import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
+import { VideosConsumers } from './videos.consumers';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
     ...Object.values(VIDEOS_PROVIDERS.REPOSITORIES),
     ...Object.values(VIDEOS_PROVIDERS.USE_CASES),
     ...Object.values(VIDEOS_PROVIDERS.HANDLERS),
+    VideosConsumers,
   ],
   //exports: [VIDEOS_PROVIDERS.REPOSITORIES.VIDEO_REPOSITORY.provide],
 })
