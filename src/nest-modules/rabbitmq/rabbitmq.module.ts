c601cd978fd01284f2c0b7f9dfbf2f7f7df1c7af
poring86 <matheuslino86a@gmail.com>
2024-08-17 16:11:03 +0000

    finish rabbitmq

--- src/nest-modules/rabbitmq/rabbitmq.module.ts
+++ src/nest-modules/rabbitmq/rabbitmq.module.ts
@@ -2,6 +2,7 @@ import { RabbitMQMessageBroker } from '@core/shared/infra/message-broker/rabbitm
 import { AmqpConnection, RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { DynamicModule } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RabbitmqConsumeErrorFilter } from './rabbitmq-consume-error/rabbitmq-consume-error.filter';
 
 export class RabbitmqModule {
   static forRoot(): DynamicModule {
@@ -11,10 +12,33 @@ export class RabbitmqModule {
         RabbitMQModule.forRootAsync(RabbitMQModule, {
           useFactory: (configService: ConfigService) => ({
             uri: configService.get('RABBITMQ_URI') as string,
+            exchanges: [
+              {
+                name: 'dlx.exchange',
+                type: 'topic',
+              },
+              {
+                name: 'direct.delayed',
+                type: 'x-delayed-message',
+                options: {
+                  arguments: {
+                    'x-delayed-type': 'direct',
+                  },
+                },
+              },
+            ],
+            queues: [
+              {
+                name: 'dlx.queue',
+                exchange: 'dlx.exchange',
+                routingKey: '#', //aceito qualquer routing key
+              },
+            ],
           }),
           inject: [ConfigService],
         }),
       ],
+      providers: [RabbitmqConsumeErrorFilter],
       global: true,
       exports: [RabbitMQModule],
     };
--- src/nest-modules/videos-module/videos.consumers.ts
+++ src/nest-modules/videos-module/videos.consumers.ts
@@ -2,18 +2,25 @@ import { AudioVideoMediaStatus } from '@core/shared/domain/value-objects/audio-v
 import { ProcessAudioVideoMediasInput } from '@core/video/application/use-cases/process-audio-video-media/process-audio-video-medias.input';
 import { ProcessAudioVideoMediasUseCase } from '@core/video/application/use-cases/process-audio-video-media/process-audio-video-medias.use-case';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
-import { Injectable, ValidationPipe } from '@nestjs/common';
+import { Injectable, UseFilters, ValidationPipe } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
+import { RabbitmqConsumeErrorFilter } from '../rabbitmq/rabbitmq-consume-error/rabbitmq-consume-error.filter';
 
+@UseFilters(RabbitmqConsumeErrorFilter)
 @Injectable()
 export class VideosConsumers {
   constructor(private moduleRef: ModuleRef) {}
 
   @RabbitSubscribe({
-    exchange: 'amq.direct',
+    exchange: 'direct.delayed',
     routingKey: 'videos.convert',
     queue: 'micro-videos/admin',
     allowNonJsonMessages: true,
+    queueOptions: {
+      deadLetterExchange: 'dlx.exchange',
+      deadLetterRoutingKey: 'videos.convert',
+      //messageTtl: 5000 - tempo de vida da mensagem na fila para ser republicada
+    },
   })
   async onProcessVideo(msg: {
     video: {
@@ -30,19 +37,16 @@ export class VideosConsumers {
       encoded_location: msg.video?.encoded_video_folder,
       status: msg.video?.status as AudioVideoMediaStatus,
     });
-    try {
-      await new ValidationPipe({
-        errorHttpStatusCode: 422,
-      }).transform(input, {
-        metatype: ProcessAudioVideoMediasInput,
-        type: 'body',
-      });
-      const useCase = await this.moduleRef.resolve(
-        ProcessAudioVideoMediasUseCase,
-      );
-      await useCase.execute(input);
-    } catch (e) {
-      console.error(e);
-    }
+
+    await new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }).transform(input, {
+      metatype: ProcessAudioVideoMediasInput,
+      type: 'body',
+    });
+    const useCase = await this.moduleRef.resolve(
+      ProcessAudioVideoMediasUseCase,
+    );
+    await useCase.execute(input);
   }
 }
